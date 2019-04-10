bbc28a2e56ba63a9690f0c258ee11218a61f6604
root <root@jmaster.localdomain>
2019-04-10 06:58:33 +0530

    Updated Java Files

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,6 @@ public class Greeter {
    * @param Some thing 
    * @return nothing 
    */
-
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
